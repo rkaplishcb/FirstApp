2c41a72356ae36270c5ba1df232fbe097fdb3110
rkaplishcb <150213688+rkaplishcb@users.noreply.github.com>
2024-10-30 20:34:50 -0400

    included a google dependency in gradle build file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.google.android.material:material:1.3.0-alpha02")
 }
